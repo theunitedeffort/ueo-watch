a0bc1e3919b52bbce9d0f169d140a304816876c0
trevorshannon <trevor.j.shannon@gmail.com>
2023-01-08 14:33:04 -0800

    Removes housing_fullhtml run commands.
    Full HTML output is not reviewed any longer, no need to waste resources on it.

--- run.sh
+++ run.sh
@@ -11,17 +11,10 @@ cd "$root_dir"
 # Run housing jobs
 urlwatch -v --hooks config/hooks.py --urls housing/urls.yaml --config housing/urlwatch.yaml --cache housing/cache.db 2> housing/urlwatch_debug.log 1> housing/urlwatch_output.log
 
-# Run housing_fullhtml jobs
-urlwatch -v --hooks config/hooks.py --urls housing_fullhtml/urls.yaml --config housing_fullhtml/urlwatch.yaml --cache housing_fullhtml/cache.db 2> housing_fullhtml/urlwatch_debug.log 1> housing_fullhtml/urlwatch_output.log
-
 # Check for errors in the housing debug log.
 body="$(grep "ERROR: " housing/urlwatch_debug.log)" && now="$(date)" && printf "Subject: urlwatch housing error [$now]\nFrom: Changebot <changebot@theunitedeffort.org>\nTo: trevor@theunitedeffort.org\n\n$body" | /sbin/sendmail -oi -t
 
-# Check for errors in the fullhtml debug log.
-body="$(grep "ERROR: " housing_fullhtml/urlwatch_debug.log)" && now="$(date)" && printf "Subject: urlwatch housing_fullhtml error [$now]\nFrom: Changebot <changebot@theunitedeffort.org>\nTo: trevor@theunitedeffort.org\n\n$body" | /sbin/sendmail -oi -t
-
 # Database garbage collection
 urlwatch  --urls housing/urls.yaml --config housing/urlwatch.yaml --cache housing/cache.db --gc-cache 2
-urlwatch  --urls housing_fullhtml/urls.yaml --config housing_fullhtml/urlwatch.yaml --cache housing_fullhtml/cache.db --gc-cache 2
 
 echo "Done."
