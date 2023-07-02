c8b63593e6a236ca98b23eab9d00a9a9e0222711
trevorshannon <trevor.j.shannon@gmail.com>
2023-07-01 22:44:07 -0700

    Improves error reporting for housing job.

--- run.sh
+++ run.sh
@@ -11,6 +11,10 @@ cd "$root_dir"
 # Run housing jobs
 urlwatch -v --hooks config/hooks.py --urls housing/urls.yaml --config housing/urlwatch.yaml --cache housing/cache.db 2> housing/urlwatch_debug.log 1> housing/urlwatch_output.log
 # Check for errors in the housing debug log.
+if [ $? -gt 0 ]
+then
+  body="$(tail -n 50 housing/urlwatch_debug.log)" && now="$(date)" && printf "Subject: urlwatch housing error [$now]\nFrom: Changebot <changebot@theunitedeffort.org>\nTo: trevor@theunitedeffort.org\n\n$body" | /sbin/sendmail -oi -t
+fi
 body="$(grep "ERROR: " housing/urlwatch_debug.log)" && now="$(date)" && printf "Subject: urlwatch housing error [$now]\nFrom: Changebot <changebot@theunitedeffort.org>\nTo: trevor@theunitedeffort.org\n\n$body" | /sbin/sendmail -oi -t
 # Database garbage collection
 urlwatch  --urls housing/urls.yaml --config housing/urlwatch.yaml --cache housing/cache.db --gc-cache 5
