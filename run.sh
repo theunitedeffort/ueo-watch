62044993a72ace1a40f2b32cbd25591af283aa04
trevorshannon <trevor.j.shannon@gmail.com>
2024-04-09 21:44:53 -0500

    Backs up cache.db locally before running.

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ root_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$root_dir"
 
 # Run housing jobs
+cp housing/cache.db housing/cache_before_latest_run.db.bak
 urlwatch -v --hooks config/hooks.py --urls housing/urls.yaml --config housing/urlwatch.yaml --cache housing/cache.db 2> housing/urlwatch_debug.log 1> housing/urlwatch_output.log
 # Check for errors in the housing debug log.
 if [ $? -gt 0 ]
@@ -20,6 +21,7 @@ body="$(grep "ERROR: " housing/urlwatch_debug.log)" && now="$(date)" && printf "
 urlwatch  --urls housing/urls.yaml --config housing/urlwatch.yaml --cache housing/cache.db --gc-cache 5
 
 # Run eligibility jobs
+cp eligibility/cache.db eligibility/cache_before_latest_run.db.bak
 urlwatch -v --hooks config/hooks.py --urls eligibility/urls.yaml --config eligibility/urlwatch.yaml --cache eligibility/cache.db 2> eligibility/urlwatch_debug.log 1> eligibility/urlwatch_output.log
 # Check for errors in the eligibility debug log.
 if [ $? -gt 0 ]
