7eb0b80258f2909cd7ea2e8bde2687c03274bf44
trevorshannon <trevor.j.shannon@gmail.com>
2024-08-16 20:40:12 -0400

    Adds autohouse jobs to run script.

--- run.sh
+++ run.sh
@@ -36,4 +36,23 @@ urlwatch  --urls eligibility/urls.yaml --config eligibility/urlwatch.yaml --cach
 timestamp=$(date +%Y%m%d_%H%M%S)
 gsutil cp eligibility/cache.db "gs://ueo-changes/eligibility/cache/cache_$timestamp.db"
 
+# Run autohouse jobs
+# First make the urls.py for urlwatch
+python3 autohouse/make_urls.py > autohouse/make_urls_debug.log 2>&1
+body="$(grep -i -B 10 "error: " autohouse/make_urls_debug.log)" && now="$(date)" && printf "Subject: autohouse make_urls error [$now]\nFrom: Changebot <changebot@theunitedeffort.org>\nTo: trevor@theunitedeffort.org\n\n$body" | /sbin/sendmail -oi -t
+
+# Then run urlwatch
+cp autohouse/cache.db autohouse/cache_before_latest_run.db.bak
+urlwatch -v --hooks config/hooks.py --urls autohouse/urls.yaml --config autohouse/urlwatch.yaml --cache autohouse/cache.db 2> autohouse/urlwatch_debug.log 1> autohouse/urlwatch_output.log
+# Check for errors in the autohouse debug log.
+if [ $? -gt 0 ]
+then
+  body="$(tail -n 50 autohouse/urlwatch_debug.log)" && now="$(date)" && printf "Subject: urlwatch autohouse error [$now]\nFrom: Changebot <changebot@theunitedeffort.org>\nTo: trevor@theunitedeffort.org\n\n$body" | /sbin/sendmail -oi -t
+fi
+body="$(grep "ERROR: " autohouse/urlwatch_debug.log)" && now="$(date)" && printf "Subject: urlwatch autohouse error [$now]\nFrom: Changebot <changebot@theunitedeffort.org>\nTo: trevor@theunitedeffort.org\n\n$body" | /sbin/sendmail -oi -t
+# Database garbage collection
+urlwatch  --urls autohouse/urls.yaml --config autohouse/urlwatch.yaml --cache autohouse/cache.db --gc-cache 5
+timestamp=$(date +%Y%m%d_%H%M%S)
+gsutil cp autohouse/cache.db "gs://ueo-changes/autohouse/cache/cache_$timestamp.db"
+
 echo "Done."
